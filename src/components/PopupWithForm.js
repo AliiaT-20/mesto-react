ed0350837d7052258b5a8430d59ae28ac6f6a2f4
AliiaT-20 <73636065+AliiaT-20@users.noreply.github.com>
2021-04-23 18:59:24 +0300

    first checked 11 work

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -3,7 +3,7 @@ function PopupWithForm(props) {
         <section className={props.isOpen ? `popup popup_type_${props.name} popup_opened` : `popup popup_type_${props.name}`}>
             <div className="popup__container">
                 <h3 className="popup__title">{props.title}</h3>
-                <form className={`popup__form popup__form_type_${props.name}`} name = {props.name}>
+                <form className={`popup__form popup__form_type_${props.name}`} name = {props.name} onSubmit={props.onSubmit}>
                     {props.children}
                 </form>
                 <button className={`popup__close-button popup__close-button_type_${props.name}`} type="button" onClick = {props.onClose}></button>
